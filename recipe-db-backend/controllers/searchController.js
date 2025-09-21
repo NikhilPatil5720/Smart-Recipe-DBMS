fdca5961fb30ea565d7707c4d5a094e60fc1c5c5
Nikhil Patil <nikhilpatil5720@gmail.com>
2025-09-21 19:07:24 +0530

    One Searchbar added

--- recipe-db-backend/controllers/searchController.js
+++ recipe-db-backend/controllers/searchController.js
@@ -45,8 +45,62 @@ const db = require('../models/db');
 
 // const db = require('../models/db');
 
+// exports.searchRecipes = (req, res) => {
+//     const { keyword, ingredient, cuisine, dietary_type } = req.query;
+
+//     let query = `
+//         SELECT DISTINCT r.*, c.name AS cuisine_name
+//         FROM Recipe r
+//         LEFT JOIN Cuisine c ON r.cuisine_id = c.cuisine_id
+//         LEFT JOIN RecipeIngredient ri ON r.recipe_id = ri.recipe_id
+//         LEFT JOIN Ingredient i ON ri.ingredient_id = i.ingredient_id
+//     `;
+
+//     const conditions = [];
+//     const params = [];
+
+//     if (keyword) {
+//         conditions.push('r.title LIKE ?');
+//         params.push(`%${keyword}%`);
+//     }
+
+//     if (ingredient) {
+//         conditions.push('i.name LIKE ?');
+//         params.push(`%${ingredient}%`);
+//     }
+
+//     if (cuisine) {
+//         conditions.push('c.name LIKE ?');
+//         params.push(`%${cuisine}%`);
+//     }
+
+//     if (dietary_type) {
+//      conditions.push('LOWER(r.dietary_type) = ?');
+//     params.push(dietary_type.toLowerCase());
+//     }
+
+//     if (conditions.length > 0) {
+//         query += ' WHERE ' + conditions.join(' AND ');  // Use AND instead of OR
+//     }
+
+//     db.query(query, params, (err, result) => {
+//         if (err) {
+//             console.error("Search query error:", err);
+//             return res.status(500).json({ error: err });
+//         }
+//         return res.json(result);
+//     });
+// };
+
+
+
+
+
+
+
+//new version with dietary_type filter
 exports.searchRecipes = (req, res) => {
-    const { keyword, ingredient, cuisine, dietary_type } = req.query;
+    const { keyword, dietary_type } = req.query;
 
     let query = `
         SELECT DISTINCT r.*, c.name AS cuisine_name
@@ -60,27 +114,18 @@ exports.searchRecipes = (req, res) => {
     const params = [];
 
     if (keyword) {
-        conditions.push('r.title LIKE ?');
-        params.push(`%${keyword}%`);
-    }
-
-    if (ingredient) {
-        conditions.push('i.name LIKE ?');
-        params.push(`%${ingredient}%`);
-    }
-
-    if (cuisine) {
-        conditions.push('c.name LIKE ?');
-        params.push(`%${cuisine}%`);
+        // Match keyword against title, cuisine name, or ingredient name
+        conditions.push('(r.title LIKE ? OR c.name LIKE ? OR i.name LIKE ?)');
+        params.push(`%${keyword}%`, `%${keyword}%`, `%${keyword}%`);
     }
 
     if (dietary_type) {
-     conditions.push('LOWER(r.dietary_type) = ?');
-    params.push(dietary_type.toLowerCase());
+        conditions.push('LOWER(r.dietary_type) = ?');
+        params.push(dietary_type.toLowerCase());
     }
 
     if (conditions.length > 0) {
-        query += ' WHERE ' + conditions.join(' AND ');  // Use AND instead of OR
+        query += ' WHERE ' + conditions.join(' AND ');
     }
 
     db.query(query, params, (err, result) => {
